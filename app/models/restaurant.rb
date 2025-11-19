73056d1adf80c54dea36c5f50fc3de0fa7bb0a4f
Margaux Schreurs <schreursmargaux@hotmail.com>
2025-11-19 14:40:06 +0000

    validations

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: {in: -> (restaurant) { restaurant.categories } }
+
+  def categories
+    %w(chinese italian japanese french belgian)
+  end
+end
